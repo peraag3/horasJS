fdeb8efced33a46d49982af347b13a350cf14c35
peraag3 <164960208+peraag3@users.noreply.github.com>
2025-01-09 21:57:10 -0300

    script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function carregar(){
     } else if (hora >= 12 && hora < 18) {
         msg.innerHTML = `São ${hora}:${minuto} horas, já está de tarde.`;
         img.src = 'tarde1.png'; 
-        document.body.style.backgroundColor = 'blue';
+        document.body.style.backgroundColor = 'ffa500';
     } else if (hora >= 18 && hora < 23) {
         msg.innerHTML = `São ${hora}:${minuto} horas, já está de noite.`;
         img.src = 'noite1.png'; 
